b56b58ba2d59e36a48fd1a3d8161907350ad2af0
Daniel Rodrigues <danielbrodrigues25@gmail.com>
2023-12-06 15:12:22 +0000

    done

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,20 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
-// Import the User model
 const User = require('../models/User.model');
-
-// Middleware to check if the user is authenticated
 const { isAuthenticated } = require('../middleware/jwt.middleware');
 
-// PUT /user/edit-profile - Updates a user's profile
 router.put('/edit-profile/', isAuthenticated, (req, res, next) => {
-    const { name, picture, email, birthday } = req.body;
-    const userId = req.payload._id; // Assuming the user's ID is stored in the JWT payload
+    const { name, picture, birthday } = req.body;
+    const userId = req.payload._id;
 
-    User.findByIdAndUpdate(userId, { name, email,picture, birthday }, { new: true })
+    User.findByIdAndUpdate(userId, { name, picture, birthday }, { new: true })
         .then(updatedUser => {
-            // Omit the password and other sensitive info in the response
             const { email, name, picture, birthday } = updatedUser;
             res.status(200).json({ email, name, picture, birthday });
         })
